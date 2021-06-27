712805a0bbc06f2932b3c329539e818e8f761710
MoTheCo <78425709+MoTheCo@users.noreply.github.com>
2021-06-27 10:14:02 +0200

    Add files via upload

---
+++ simple calc.py
@@ -0,0 +1,12 @@
+a = int(input("gib eine zahl ein "))
+b = int(input("gib eine zweite zahl ein "))
+c = a + b
+d = int(input("wie oft soll die zahl multipliziert werden? "))
+e = c * d
+print("deine beiden zahlen ergeben zusammen =")
+print(c)
+print("deine zahlen ergeben multipliziert =")
+print(e)
+print("und das mal sich multipliziert =")
+f = e * e
+print(f)
